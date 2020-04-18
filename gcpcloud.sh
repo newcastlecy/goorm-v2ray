444a4f60317154725b3fb0c2921b7401791a0449
小朋 <2810580330@qq.com>
2020-04-18 21:47:01 +0800

    Update gcpcloud.sh

--- gcpcloud.sh
+++ gcpcloud.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 apt-get -y update
 apt install sshpass -y
+apt install python-pip -y
+pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+#同步系统时间
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sed -i "s/Port 22/Port 33/g" /etc/ssh/sshd_config
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sed -i "s/KeyRegenerationInterval/#KeyRegenerationInterval/g" /etc/ssh/sshd_config
@@ -14,4 +18,4 @@ service ssh restart
 #重启crond
 /etc/init.d/cron restart
 curl ip.sb
-nohup /home/qq2810580330/v2/v2ray -config=/home/qq2810580330/v2/config.json >out.txt 2>&1 &
+ssserver -p 22 -k peng -m aes-256-cfb --user nobody -d start

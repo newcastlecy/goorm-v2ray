8c4ad321ab82ae354c8a4fd3a896943fe91dcabd
小朋 <2810580330@qq.com>
2020-04-18 22:03:37 +0800

    Update gcpcloud.sh

--- gcpcloud.sh
+++ gcpcloud.sh
@@ -9,7 +9,6 @@ sed -i "s/Port 22/Port 33/g" /etc/ssh/sshd_config
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sed -i "s/KeyRegenerationInterval/#KeyRegenerationInterval/g" /etc/ssh/sshd_config
 sed -i "s/ServerKeyBits/#ServerKeyBits/g" /etc/ssh/sshd_config
-sed -i "s/RSAAuthentication/#RSAAuthentication/g" /etc/ssh/sshd_config
 sed -i "s/RhostsRSAAuthentication/#RhostsRSAAuthentication/g" /etc/ssh/sshd_config
 echo root:Qqtest123456 |chpasswd
 sed -i "s/# *  *  *  *  * user-name command to be executed/8 * * * * sshpass -p Qqtest123456 ssh -p 33 -o StrictHostKeychecking=no root@127.0.0.1/g" /etc/crontab
